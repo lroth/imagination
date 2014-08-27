d30a20ea4564a3d9bd2fb01646fe21cd58368fe7
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-27 16:47:58 +0200

    cleanup stars

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,40 +1,10 @@
-//App definition
+
 var App = (function ($, undefined) {
-
-    // local variable
-    var localVariable;
-
-    //sample function
-    var sampleFunction = function() {
-       //sample function code
-       console.log("sample");
-    };
-
     return {
         //main method to initiate template pages
         init: function () {
-            //call local function
-            sampleFunction()
-
-            //call local method
-            this.sampleMethod('test');
+            this.stars.init();
         },
-
-        //sample method declaration
-        sampleMethod: function (test)
-        {
-            console.log(test);
-        },
-
-        open: function (url)
-        {
-            window.location.href = url;
-        }
     };
 
 }(jQuery));
-
-//App initialize
-jQuery(document).ready(function(){
-    App.init();
-});
