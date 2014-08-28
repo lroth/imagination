f0409acf5254ecf8f199b530f2d3e2897b23d1c9
Adam Misiorny <adam.misiorny@gmail.com>
2014-08-28 15:11:46 +0200

    add most of head and some animations

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,9 +1,67 @@
 
 var App = (function ($, undefined) {
+
+    var objects = {
+        head: $('#head'),
+        hair: $('#hair'),
+        eyeLeft: $('#eye-left'),
+        eyeRight: $('#eye-right'),
+        eyeballLeft: $('#eyeball-left'),
+        eyeballRight: $('#eyeball-right'),
+        ears: $('#ears')
+    };
+
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    function rotateEyeballRight() {
+        TweenLite.to(objects.eyeballRight, 1, {rotation:"+=360", transformOrigin: '70px 66px', onComplete:rotateEyeballRight});
+    }
+
+    function rotateEyeRight() {
+        TweenLite.to(objects.eyeRight, 1, {rotation:"+=20", transformOrigin: '70px 66px'});
+        TweenLite.to(objects.eyeRight, 1, {rotation:"-=20", transformOrigin: '70px 66px', delay:1, onComplete:rotateEyeRight});
+    }
+
+    function earsWiggle() {
+        TweenLite.to(objects.ears, 0.5, {rotation:"+=3", transformOrigin: '305px 96px'});
+        TweenLite.to(objects.ears, 0.5, {rotation:"-=3", transformOrigin: '305px 96px', delay:3, onComplete:earsWiggle});
+    }
+
+    function headWiggle() {
+        var range = getRandomInt(1, 5);
+        var speed = getRandomInt(1, 3);
+        var delay = getRandomInt(1, 3);
+        TweenLite.to(objects.head, speed, {rotation:'+=' + range, transformOrigin: '350px 730px'});
+        TweenLite.to(objects.head, speed, {rotation:'-=' + range, transformOrigin: '350px 730px', delay:delay , onComplete:headWiggle});
+    }
+
+    function hairWiggle() {
+        var range = getRandomInt(1, 5);
+        var speed = getRandomInt(1, 3);
+        var delay = getRandomInt(1, 3);
+        TweenLite.to(objects.hair, speed, {rotation:'+=' + range, transformOrigin: '283px 242px'});
+        TweenLite.to(objects.hair, speed, {rotation:'-=' + range, transformOrigin: '283px 242px', delay:delay , onComplete:hairWiggle});
+    }
+
+    function animate() {
+
+        rotateEyeballRight();
+        rotateEyeRight();
+        headWiggle();
+        hairWiggle();
+        earsWiggle();
+
+        // TweenMax.to($('#eyeball-right') , 2, {rotation:360, transformOrigin:"70px 66px", repeat:true, repeatDelay:0, ease:Linear.easeNone});
+
+    }
+
     return {
         //main method to initiate template pages
         init: function () {
             this.stars.init();
+            animate();
         },
     };
 
